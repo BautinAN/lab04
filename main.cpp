2ea6cfbfddb06633c9040aae41354fb52d3ecf52
BautinAN <bautinalexey@yandex.ru>
2021-10-04 20:03:53 +0300

    3.2.6 выполнена

--- main.cpp
+++ main.cpp
@@ -8,9 +8,26 @@
 using namespace std;
 
 
+int
+main(int argc, char* argv[])
+{
+    if (argc > 1)
+        for (size_t i = 0; i < argc; i++)
+        {
+            CURL *curl = curl_easy_init();
+if(curl) {
+  CURLcode res;
+  curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+  res = curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
+}
 
-int main()
-{   curl_global_init(CURL_GLOBAL_ALL);
+            return 0;
+        }
+
+
+
+    curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins, input);
